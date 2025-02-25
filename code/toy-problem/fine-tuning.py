92f24d91dd831793781aa405b9f2f1ab9fe82528
Aryan Sajith <86263632+Aryan-Sajith@users.noreply.github.com>
2025-02-24 19:15:36 -0500

    Loaded full dataset without shuffling to properly index features for fine-tuning

--- code/toy-problem/fine-tuning.py
+++ code/toy-problem/fine-tuning.py
@@ -9,3 +9,16 @@ features_path = 'code/toy-problem/features/self_supervised_features.npy'
 # --- Load Features from Self-Supervised Pre-Training ---
 features = np.load(features_path)
 print("Loaded features with shape:", features.shape)
+
+# --- Load Full Dataset without Shuffling to Properly Index Features ---
+full_ds = tf.keras.utils.image_dataset_from_directory(
+    dataset_path,
+    labels='inferred',
+    label_mode='categorical',  
+    image_size=(250, 250),
+    batch_size=32,
+    shuffle=False, # Do not shuffle to properly index features
+    seed=42
+)
+all_class_names = full_ds.class_names
+print("All classes in dataset:", all_class_names)
